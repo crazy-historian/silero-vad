50e2ea821692916730df803b1830ed759984afb0
maxim <zaitsev808@mail.ru>
2023-09-17 21:42:12 +0300

    started to make onnx wrapper simpler

--- utils_vad.py
+++ utils_vad.py
@@ -1,18 +1,98 @@
+
 import torch
 import torchaudio
-from typing import Callable, List
 import torch.nn.functional as F
+
+from typing import Callable, List
+
 import warnings
+import numpy as np
+import onnxruntime
+
 
 languages = ['ru', 'en', 'de', 'es']
 
 
+
+def get_onnx_inference_session(session_options, onnx_filepath: str, on_cpu: bool = True, num_threads: int = 1):
+    if on_cpu and 'CPUExecutionProvider' in onnxruntime.get_available_providers():
+        session = onnxruntime.InferenceSession(
+            onnx_filepath,
+            providers=['CPUExecutionProvider'],
+            sess_options=session_options
+            )
+    else:
+        session = onnxruntime.InferenceSession(onnx_filepath, sess_options=session_options)
+
+    session_options.inter_op_num_threads = num_threads
+    session_options.intra_op_num_threads = num_threads
+
+
+    return session
+    
+def input_is_valid():
+    if x.dim() == 1:
+        x = x.unsqueeze(0)
+    if x.dim() > 2:
+        raise ValueError(f"Too many dimensions for input audio chunk {x.dim()}")
+
+    if sr != 16000 and (sr % 16000 == 0):
+        step = sr // 16000
+        x = x[:,::step]
+        sr = 16000
+
+    if sr not in self.sample_rates:
+        raise ValueError(f"Supported sampling rates: {self.sample_rates} (or multiply of 16000)")
+
+    if sr / x.shape[1] > 31.25:
+        raise ValueError("Input audio chunk is too short")
+
+    return x, sr
+
+
+class VoiceActivityDetection:
+    def __init__(self, samplerate: int, onnx_filepath: str, num_threads: int):
+        self.samplerate = samplerate
+        self.session = get_onnx_inference_session(onnxruntime.SessionOptions(), onnx_filepath, on_cpu=True, num_threads=num_threads)
+        
+        self._h = np.zeros((2, 1, 64)).astype('float32')
+        self._c = np.zeros((2, 1, 64)).astype('float32')
+        self._last_sr = 0
+        self._last_batch_size = 0
+
+    def _reset(self, batch_size: int):
+        self._h = np.zeros((2, batch_size, 64)).astype('float32')
+        self._c = np.zeros((2, batch_size, 64)).astype('float32')
+        self._last_sr = 0
+        self._last_batch_size = 0
+
+    def inference(self):
+        x, sr = self._validate_input(x, sr)
+        batch_size = x.shape[0]
+
+        if not self._last_batch_size:
+            self.reset_states(batch_size)
+        if (self._last_sr) and (self._last_sr != sr):
+            self.reset_states(batch_size)
+        if (self._last_batch_size) and (self._last_batch_size != batch_size):
+            self.reset_states(batch_size)
+
+        if sr in [8000, 16000]:
+            ort_inputs = {'input': x.numpy(), 'h': self._h, 'c': self._c, 'sr': np.array(sr, dtype='int64')}
+            ort_outs = self.session.run(None, ort_inputs)
+            out, self._h, self._c = ort_outs
+        else:
+            raise ValueError()
+
+        self._last_sr = sr
+        self._last_batch_size = batch_size
+
+        out = torch.tensor(out)
+        return out
+
 class OnnxWrapper():
 
     def __init__(self, path, force_onnx_cpu=False):
-        import numpy as np
-        global np
-        import onnxruntime
 
         opts = onnxruntime.SessionOptions()
         opts.inter_op_num_threads = 1
