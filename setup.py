9ef00f45d99685f34359d4e3e0e50c077921d997
maxim <zaitsev808@mail.ru>
2023-09-30 20:25:49 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from pathlib import Path
+
+project_path = Path(__file__).parent
+
+README = str(Path(f'{project_path}/README.md'))
+with open(README, encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='silero_vad_simplified',
+    version='0.1.0',    
+    description='This is a fork of silero-vad repository with a bit refactored code for simpler installation.',
+    long_description=long_description,
+    license='MIT License',
+    long_desctiption_content_type='text/markdown',
+    url='https://github.com/crazy-historian/silero-vad',
+    author='Maxim Zaitsev',
+    author_email='zaitsev808@mail.ru',
+    packages=['silero_vad', 'silero_vad/files'],
+    package_data={
+        'silero_vad/files': ['lang_dict_95.json', 'lang_dict_95.json', 'silero_vad.jit', 'silero_vad.onnx']},
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'torch',
+        'torchaudio',
+        'onnxruntime'
+        ],
+)
